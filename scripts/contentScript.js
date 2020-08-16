9de57154196ce8f150b34eeee720a7d666bb1fa9
Andrew An <anandrew1995@gmail.com>
2020-08-16 13:54:24 -0400

    Should only filter the best view

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,39 +1,45 @@
-$('.fm_best_widget').find('li').each(function () {
-    const href = $(this).find('.category').children('a').attr('href');
-    const key = `fmkFilter::${href}`;
-    chrome.storage.sync.get([key, 'fmkFilter::filterMode'], (result) => {
-        if (!result[key]) {
-            if (result['fmkFilter::filterMode'] === 'blur') {
-                blur($(this));
-                $(this).find('.title').find('a')
-                    .mouseenter(() => { unblur($(this)) })
-                    .mouseleave(() => { blur($(this)) });
+if (shouldFilterByPath(window.location.search)) {
+    $('.fm_best_widget').find('li').each(function () {
+        const href = $(this).find('.category').children('a').attr('href');
+        const key = `fmkFilter::${href}`;
+        chrome.storage.sync.get([key, 'fmkFilter::filterMode'], (result) => {
+            if (!result[key]) {
+                if (result['fmkFilter::filterMode'] === 'blur') {
+                    blur($(this));
+                    $(this).find('.title').find('a')
+                        .mouseenter(() => { unblur($(this)) })
+                        .mouseleave(() => { blur($(this)) });
+                }
+                else if (result['fmkFilter::filterMode'] === 'hide') {
+                    $(this).hide();
+                }
             }
-            else if (result['fmkFilter::filterMode'] === 'hide') {
-                $(this).hide();
-            }
-        }
+        });
     });
-});
 
-findAnFilterKeyword('ul', 'li.li', 'h3');
-findAnFilterKeyword('tbody', 'tr', 'td');
+    findAndFilterKeyword('ul', 'li.li', 'h3');
+    findAndFilterKeyword('tbody', 'tr', 'td');
+}
 
-function findAnFilterKeyword(listElemType, postElemType, titleElemType) {
+
+
+function findAndFilterKeyword(listElemType, postElemType, titleElemType) {
     $(listElemType).find(postElemType).each(function () {
         const title = $(this).find(`${titleElemType}.title`).children('a').text();
         const key = `fmkFilter::keywords`;
         chrome.storage.sync.get([key, 'fmkFilter::filterMode'], (result) => {
-            for ([keyword, enabled] of Object.entries(result[key])) {
-                if (enabled && title.includes(keyword)) {
-                    if (result['fmkFilter::filterMode'] === 'blur') {
-                        blur($(this));
-                        $(this).find('.title').find('a')
-                            .mouseenter(() => { unblur($(this)) })
-                            .mouseleave(() => { blur($(this)) });
-                    }
-                    else if (result['fmkFilter::filterMode'] === 'hide') {
-                        $(this).hide();
+            if (result[key]) {
+                for ([keyword, enabled] of Object.entries(result[key])) {
+                    if (enabled && title.includes(keyword)) {
+                        if (result['fmkFilter::filterMode'] === 'blur') {
+                            blur($(this));
+                            $(this).find('.title').find('a')
+                                .mouseenter(() => { unblur($(this)) })
+                                .mouseleave(() => { blur($(this)) });
+                        }
+                        else if (result['fmkFilter::filterMode'] === 'hide') {
+                            $(this).hide();
+                        }
                     }
                 }
             }
@@ -42,11 +48,11 @@ function findAnFilterKeyword(listElemType, postElemType, titleElemType) {
 }
 
 function blur(elem) {
-    elem.css('-webkit-filter', 'blur(2px)');
-    elem.css('-moz-filter', 'blur(2px)');
-    elem.css('-o-filter', 'blur(2px)');
-    elem.css('-ms-filter', 'blur(2px)');
-    elem.css('filter', 'blur(2px)');
+    elem.css('-webkit-filter', 'blur(3px)');
+    elem.css('-moz-filter', 'blur(3px)');
+    elem.css('-o-filter', 'blur(3px)');
+    elem.css('-ms-filter', 'blur(3px)');
+    elem.css('filter', 'blur(3px)');
 }
 
 function unblur(elem) {
@@ -55,4 +61,8 @@ function unblur(elem) {
     elem.css('-o-filter', 'blur(0px)');
     elem.css('-ms-filter', 'blur(0px)');
     elem.css('filter', 'blur(0px)');
+}
+
+function shouldFilterByPath(search) {
+    return search === '' || search.startsWith('?mid=best')
 }
